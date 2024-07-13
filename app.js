782eaee43266211c6a0388fb3324d4eabc1c5471
Bruno <bruno.gmouro@gmail.com>
2024-07-13 06:49:45 -0300

    Revert "Alterando as tentativas"
    This reverts commit 778f6b7faf2298e402333b3393be44713706d008.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let listaDeNumerosSorteados = [];
 let numeroLimite = 30;
 let numeroSecreto = gerarNumeroAleatorio();
-let tentativas = 5;
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
